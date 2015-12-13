6b2a50bd1e614d7d6a19a222b5b1248e201528d2
Ben <b.cylin@gmail.com>
2015-12-13 21:32:42 +0800

    Render LanguagesView with ListView

--- ReactComponent/LanguagesView.js
+++ ReactComponent/LanguagesView.js
@@ -2,28 +2,60 @@
 
 var React = require('react-native');
 var {
-  View,
+  ListView,
   Text,
+  TouchableHighlight,
+  View
 } = React;
 
 var styles = React.StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'gray'
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
   },
-  text: {
-    color: 'white',
-    textAlign: 'center'
+  title: {
+    fontSize: 17
+  },
+  target: {
+    fontSize: 17,
+    color: 'blue'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: 'lightGray'
   }
 });
 
 class LanguagesView extends React.Component {
+
+  constructor(props) {
+    super(props);
+    let dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: dataSource.cloneWithRows(['Swift', 'Objective-C', 'JavaScript'])
+    };
+  }
+
+  renderRow(rowData, sectionID, rowID) {
+    return (
+      <TouchableHighlight underlayColor='lightGray'>
+        <View>
+          <View style={styles.rowContainer}>
+            <Text style={(rowData === 'JavaScript' ? styles.target : styles.title)} numberOfLines={1}>
+              {rowData}
+            </Text>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>This will be LanguagesView.</Text>
-      </View>
-    )
+      <ListView dataSource={this.state.dataSource}
+                renderRow={this.renderRow.bind(this)} />
+    );
   }
 }
 
